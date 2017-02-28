67199dc8423da849afe3cb80fa63bb5e56fffffa
Elior <elior.b@dynamicyield.com>
2017-02-28 22:23:00 +0200

    revert latest changes for not EAPs

--- src/main/java/com/chrisrm/idea/MTTheme.java
+++ src/main/java/com/chrisrm/idea/MTTheme.java
@@ -89,9 +89,9 @@ public enum MTTheme {
 
         UIDefaults uiDefaults = UIManager.getLookAndFeelDefaults();
 
-        if (uiSettings.getOverrideLafFonts()) {
+        if (uiSettings.OVERRIDE_NONIDEA_LAF_FONTS) {
 //            JBUI.setScaleFactor(uiSettings.FONT_SIZE / 12f);
-            initFontDefaults(uiDefaults, uiSettings.getFontFace(), uiSettings.getFontSize());
+            initFontDefaults(uiDefaults, uiSettings.FONT_FACE, uiSettings.FONT_SIZE);
         }
     }
 
