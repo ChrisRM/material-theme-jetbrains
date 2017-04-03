6470919fd14609bd51e4f4281ef7ce15460b67af
mallowigi <heliosaian@gmail.com>
2017-04-03 21:18:10 +0300

    patch painter refactor

--- src/main/java/com/chrisrm/idea/MTTabsPainterPatcher.java
+++ src/main/java/com/chrisrm/idea/MTTabsPainterPatcher.java
@@ -9,6 +9,7 @@ import com.intellij.openapi.fileEditor.FileEditorManagerListener;
 import com.intellij.openapi.util.registry.Registry;
 import com.intellij.ui.ColorUtil;
 import com.intellij.ui.Gray;
+import com.intellij.ui.JBColor;
 import com.intellij.ui.tabs.JBTabsPosition;
 import com.intellij.ui.tabs.impl.*;
 import com.intellij.util.ReflectionUtil;
@@ -94,55 +95,7 @@ public class MTTabsPainterPatcher implements ApplicationComponent {
         int borderThickness = config.getHighlightThickness();
 
         if ("paintSelectionAndBorder".equals(method.getName())) {
-          // Get the shapeinfo class because it is protected
-          Class<?> clazz = Class.forName("com.intellij.ui.tabs.impl.JBTabsImpl$ShapeInfo");
-
-          // Retrieve arguments
-          final Graphics2D g2d = (Graphics2D) objects[0];
-          final Rectangle rect = (Rectangle) objects[1];
-          Object selectedShape = objects[2];
-          final Insets insets = (Insets) objects[3];
-          final Color tabColor = (Color) objects[4];
-
-          // Retrieve private fields of ShapeInfo class
-          Field fillPathField = clazz.getField("fillPath");
-          ShapeTransform fillPath = (ShapeTransform) fillPathField.get(selectedShape);
-          Field labelPathField = clazz.getField("labelPath");
-          ShapeTransform labelPath = (ShapeTransform) labelPathField.get(selectedShape);
-          Field pathField = clazz.getField("path");
-          ShapeTransform path = (ShapeTransform) pathField.get(selectedShape);
-
-          // Other properties needed for drawing
-          int _x = rect.x;
-          int _y = rect.y;
-          int _height = rect.height;
-          Insets i = path.transformInsets(insets);
-          int thickness = borderThickness;
-
-          // The tabs component
-          JBEditorTabs tabsComponent = tabsPainter.getTabsComponent();
-
-          // Position of tabs
-          JBTabsPosition position = tabsComponent.getTabsPosition();
-          boolean horizontalTabs = tabsPainter.isHorizontalTabs();
-
-          // color me
-          tabsPainter.fillSelectionAndBorder(g2d, fillPath, tabColor, _x, _y, _height);
-          g2d.setColor(borderColor);
-
-          if (position == JBTabsPosition.bottom) {
-            // Paint on top
-            g2d.fillRect(rect.x, rect.y - 1, rect.width, thickness);
-          } else if (position == JBTabsPosition.top) {
-            // Paint on bottom
-            g2d.fillRect(rect.x, rect.y + rect.height - thickness + 1, rect.width, thickness);
-            g2d.setColor(UIUtil.CONTRAST_BORDER_COLOR);
-            g2d.drawLine(Math.max(0, rect.x - 1), rect.y, rect.x + rect.width, rect.y);
-          } else if (position == JBTabsPosition.left) {
-            g2d.fillRect(rect.x + rect.width - thickness + 1, rect.y, thickness, rect.height);
-          } else if (position == JBTabsPosition.right) {
-            g2d.fillRect(rect.x, rect.y, thickness, rect.height);
-          }
+          paintSelectionAndBorder(objects, borderColor, borderThickness, tabsPainter);
         }
 
         return result;
@@ -150,6 +103,62 @@ public class MTTabsPainterPatcher implements ApplicationComponent {
     });
 
     ReflectionUtil.setField(JBEditorTabs.class, component, JBEditorTabsPainter.class, "myDarkPainter", proxy);
+  }
+
+  private void paintSelectionAndBorder(Object[] objects,
+                                       Color borderColor,
+                                       int borderThickness,
+                                       MTTabsPainter tabsPainter)
+      throws ClassNotFoundException, NoSuchFieldException, IllegalAccessException {
+    // Get the shapeinfo class because it is protected
+    Class<?> clazz = Class.forName("com.intellij.ui.tabs.impl.JBTabsImpl$ShapeInfo");
+
+    // Retrieve arguments
+    final Graphics2D g2d = (Graphics2D) objects[0];
+    final Rectangle rect = (Rectangle) objects[1];
+    Object selectedShape = objects[2];
+    final Insets insets = (Insets) objects[3];
+    final Color tabColor = (Color) objects[4];
+
+    // Retrieve private fields of ShapeInfo class
+    Field fillPathField = clazz.getField("fillPath");
+    ShapeTransform fillPath = (ShapeTransform) fillPathField.get(selectedShape);
+    Field labelPathField = clazz.getField("labelPath");
+    ShapeTransform labelPath = (ShapeTransform) labelPathField.get(selectedShape);
+    Field pathField = clazz.getField("path");
+    ShapeTransform path = (ShapeTransform) pathField.get(selectedShape);
+
+    // Other properties needed for drawing
+    int _x = rect.x;
+    int _y = rect.y;
+    int _height = rect.height;
+    Insets i = path.transformInsets(insets);
+    int thickness = borderThickness;
+
+    // The tabs component
+    JBEditorTabs tabsComponent = tabsPainter.getTabsComponent();
+
+    // Position of tabs
+    JBTabsPosition position = tabsComponent.getTabsPosition();
+    boolean horizontalTabs = tabsPainter.isHorizontalTabs();
+
+    // color me
+    tabsPainter.fillSelectionAndBorder(g2d, fillPath, tabColor, _x, _y, _height);
+    g2d.setColor(borderColor);
+
+    if (position == JBTabsPosition.bottom) {
+      // Paint on top
+      g2d.fillRect(rect.x, rect.y - 1, rect.width, thickness);
+    } else if (position == JBTabsPosition.top) {
+      // Paint on bottom
+      g2d.fillRect(rect.x, rect.y + rect.height - thickness + 1, rect.width, thickness);
+      g2d.setColor(UIUtil.CONTRAST_BORDER_COLOR);
+      g2d.drawLine(Math.max(0, rect.x - 1), rect.y, rect.x + rect.width, rect.y);
+    } else if (position == JBTabsPosition.left) {
+      g2d.fillRect(rect.x + rect.width - thickness + 1, rect.y, thickness, rect.height);
+    } else if (position == JBTabsPosition.right) {
+      g2d.fillRect(rect.x, rect.y, thickness, rect.height);
+    }
   }
 
   @Override
