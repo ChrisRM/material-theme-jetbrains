e528e28794fb8b588fc312758fd3b0de34200209
mallowigi <heliosaian@gmail.com>
2017-04-09 00:23:41 +0300

    remove annoying messages

--- src/main/java/com/chrisrm/idea/actions/MTDarkerTheme.java
+++ src/main/java/com/chrisrm/idea/actions/MTDarkerTheme.java
@@ -4,13 +4,10 @@ import com.chrisrm.idea.MTTheme;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 
-import javax.swing.*;
-
 
 public class MTDarkerTheme extends AnAction {
     @Override
     public void actionPerformed(AnActionEvent anActionEvent) {
-        JOptionPane.showMessageDialog(null, "Keep in mind, the darker theme is a work in progress and there will be some issues.", "Work in progress", JOptionPane.INFORMATION_MESSAGE);
         MTTheme.DARKER.activate();
     }
 }
