ae6a393804054b94c7eef6f584c2ed99f1f200b8
mallowigi <heliosaian@gmail.com>
2017-05-31 18:52:12 +0300

    convert to jformdesigner

--- src/main/java/com/chrisrm/idea/config/ui/MTForm.java
+++ src/main/java/com/chrisrm/idea/config/ui/MTForm.java
@@ -3,37 +3,23 @@ package com.chrisrm.idea.config.ui;
 import com.chrisrm.idea.MTConfig;
 import com.chrisrm.idea.MTTheme;
 import com.chrisrm.idea.messages.MaterialThemeBundle;
+import com.intellij.uiDesigner.core.GridConstraints;
+import com.intellij.uiDesigner.core.GridLayoutManager;
+import com.intellij.uiDesigner.core.Spacer;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
 import java.awt.*;
+import java.util.ResourceBundle;
 
 public class MTForm implements MTFormUI {
-  private CheckBoxWithColorChooserImpl checkBoxWithColorChooserImpl;
-  private JPanel content;
-  private JSpinner highlightSpinner;
-  private JButton reset;
-  private JCheckBox isContrastModeCheckbox;
   private SpinnerModel highlightSpinnerModel;
-  private JCheckBox isMaterialDesignCheckbox;
-  private JCheckBox isWallpaperSetCheckbox;
 
-  private JCheckBox boldTabs;
-
-  public MTForm() {
-
-    reset.addActionListener(e -> {
-      final MTTheme mtTheme = MTConfig.getInstance().getSelectedTheme();
-      Color borderColor = mtTheme.getBorderColor();
-      int thickness = mtTheme.getBorderThickness();
-
-      this.setHighlightColor(borderColor);
-      this.setHighlightColorEnabled(false);
-      this.setHighlightThickness(thickness);
-      this.setIsBoldTabs(false);
-      this.setIsWallpaperSet(true);
-    });
-  }
+  // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+  // Generated using JFormDesigner Evaluation license - Mario Smilax
+  private JPanel content;
 
   @Override
   public void init() {
@@ -105,15 +91,216 @@ public class MTForm implements MTFormUI {
     this.boldTabs.setSelected(isBold);
   }
 
-  public boolean getIsWallpaperSet() {
-    return isWallpaperSetCheckbox.isSelected();
-  }
-
-  public void setIsWallpaperSet(boolean isWallPaperSet) {
-    this.isWallpaperSetCheckbox.setSelected(isWallPaperSet);
-  }
+  private CheckBoxWithColorChooserImpl checkBoxWithColorChooserImpl;
+  private JSpinner highlightSpinner;
 
   private void createUIComponents() {
     checkBoxWithColorChooserImpl = new CheckBoxWithColorChooserImpl(MaterialThemeBundle.message("mt.activetab.text"));
   }
+
+  private JButton reset;
+  private JCheckBox boldTabs;
+  private JCheckBox isContrastModeCheckbox;
+  private JCheckBox isWallpaperSetCheckbox;
+  private JCheckBox isMaterialDesignCheckbox;
+
+  public MTForm() {
+
+    initComponents();
+    reset.addActionListener(e -> {
+      final MTTheme mtTheme = MTConfig.getInstance().getSelectedTheme();
+      Color borderColor = mtTheme.getBorderColor();
+      int thickness = mtTheme.getBorderThickness();
+
+      this.setHighlightColor(borderColor);
+      this.setHighlightColorEnabled(false);
+      this.setHighlightThickness(thickness);
+      this.setIsBoldTabs(false);
+    });
+  }
+
+  public boolean getIsWallpaperSet() {
+    return this.isWallpaperSetCheckbox.isSelected();
+  }
+
+  public void setIsWallpaperSet(boolean isWallpaperSet) {
+    this.isWallpaperSetCheckbox.setSelected(isWallpaperSet);
+  }
+
+  private void initComponents() {
+    // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+    // Generated using JFormDesigner Evaluation license - Mario Smilax
+    createUIComponents();
+
+    ResourceBundle bundle = ResourceBundle.getBundle("messages.MaterialThemeBundle");
+    content = new JPanel();
+    JPanel panel1 = new JPanel();
+    Spacer hSpacer1 = new Spacer();
+    JLabel label1 = new JLabel();
+    highlightSpinner = new JSpinner();
+    Spacer vSpacer1 = new Spacer();
+    reset = new JButton();
+    boldTabs = new JCheckBox();
+    Spacer vSpacer2 = new Spacer();
+    JPanel panel2 = new JPanel();
+    isContrastModeCheckbox = new JCheckBox();
+    Spacer hSpacer2 = new Spacer();
+    Spacer vSpacer3 = new Spacer();
+    JPanel panel3 = new JPanel();
+    isWallpaperSetCheckbox = new JCheckBox();
+    Spacer hSpacer3 = new Spacer();
+    isMaterialDesignCheckbox = new JCheckBox();
+
+    //======== content ========
+    {
+
+      // JFormDesigner evaluation mark
+      content.setBorder(new javax.swing.border.CompoundBorder(
+          new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder(0, 0, 0, 0),
+              "JFormDesigner Evaluation", javax.swing.border.TitledBorder.CENTER,
+              javax.swing.border.TitledBorder.BOTTOM, new java.awt.Font("Dialog", java.awt.Font.BOLD, 12),
+              java.awt.Color.red), content.getBorder()));
+
+      content.setLayout(new GridLayoutManager(5, 1, new Insets(0, 0, 0, 0), -1, -1));
+
+      //======== panel1 ========
+      {
+        panel1.setBorder(new TitledBorder(new EtchedBorder(), bundle.getString("mt.activetab")));
+        panel1.setLayout(new GridLayoutManager(5, 2, new Insets(0, 0, 0, 0), -1, -1));
+
+        //---- checkBoxWithColorChooserImpl ----
+        checkBoxWithColorChooserImpl.setDoubleBuffered(true);
+        checkBoxWithColorChooserImpl.setToolTipText(bundle.getString("mt.activetab.highlight.tooltip"));
+        panel1.add(checkBoxWithColorChooserImpl, new GridConstraints(0, 0, 1, 1,
+            GridConstraints.ANCHOR_NORTHWEST, GridConstraints.FILL_NONE,
+            GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+            GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW | GridConstraints.SIZEPOLICY_WANT_GROW,
+            null, new Dimension(204, 18), null));
+        panel1.add(hSpacer1, new GridConstraints(0, 1, 1, 1,
+            GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL,
+            GridConstraints.SIZEPOLICY_CAN_GROW | GridConstraints.SIZEPOLICY_WANT_GROW,
+            GridConstraints.SIZEPOLICY_CAN_SHRINK,
+            null, new Dimension(14, 28), null));
+
+        //---- label1 ----
+        label1.setHorizontalTextPosition(SwingConstants.LEADING);
+        label1.setLabelFor(highlightSpinner);
+        label1.setText(bundle.getString("mt.border.thickness"));
+        label1.setToolTipText(bundle.getString("mt.border.thickness.tooltip"));
+        panel1.add(label1, new GridConstraints(1, 0, 1, 1,
+            GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE,
+            GridConstraints.SIZEPOLICY_FIXED,
+            GridConstraints.SIZEPOLICY_FIXED,
+            null, new Dimension(204, 18), null));
+        panel1.add(highlightSpinner, new GridConstraints(1, 1, 1, 1,
+            GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE,
+            GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+            GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+            null, new Dimension(89, 29), null));
+        panel1.add(vSpacer1, new GridConstraints(4, 0, 1, 1,
+            GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL,
+            GridConstraints.SIZEPOLICY_CAN_SHRINK,
+            GridConstraints.SIZEPOLICY_CAN_GROW | GridConstraints.SIZEPOLICY_WANT_GROW,
+            null, null, null));
+
+        //---- reset ----
+        reset.setText(bundle.getString("mt.resetdefaults"));
+        reset.setToolTipText(bundle.getString("mt.resetdefaults.tooltip"));
+        panel1.add(reset, new GridConstraints(3, 0, 1, 1,
+            GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL,
+            GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+            GridConstraints.SIZEPOLICY_FIXED,
+            null, null, null));
+
+        //---- boldTabs ----
+        boldTabs.setLabel(bundle.getString("mt.boldtabs"));
+        boldTabs.setText(bundle.getString("mt.boldtabs"));
+        boldTabs.setToolTipText(bundle.getString("mt.boldtabs.tooltip"));
+        panel1.add(boldTabs, new GridConstraints(2, 0, 1, 1,
+            GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE,
+            GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+            GridConstraints.SIZEPOLICY_FIXED,
+            null, null, null));
+      }
+      content.add(panel1, new GridConstraints(0, 0, 1, 1,
+          GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH,
+          GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+          GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+          null, null, null));
+      content.add(vSpacer2, new GridConstraints(3, 0, 1, 1,
+          GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL,
+          GridConstraints.SIZEPOLICY_CAN_SHRINK,
+          GridConstraints.SIZEPOLICY_CAN_GROW | GridConstraints.SIZEPOLICY_WANT_GROW,
+          null, null, null));
+
+      //======== panel2 ========
+      {
+        panel2.setBorder(new TitledBorder(new EtchedBorder(), bundle.getString("mt.panels.section")));
+        panel2.setLayout(new GridLayoutManager(3, 2, new Insets(0, 0, 0, 0), -1, -1));
+
+        //---- isContrastModeCheckbox ----
+        isContrastModeCheckbox.setLabel(bundle.getString("mt.contrast"));
+        isContrastModeCheckbox.setText(bundle.getString("mt.contrast"));
+        isContrastModeCheckbox.setToolTipText(bundle.getString("mt.contrast.tooltip"));
+        panel2.add(isContrastModeCheckbox, new GridConstraints(0, 0, 1, 1,
+            GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE,
+            GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+            GridConstraints.SIZEPOLICY_FIXED,
+            null, null, null));
+        panel2.add(hSpacer2, new GridConstraints(0, 1, 1, 1,
+            GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL,
+            GridConstraints.SIZEPOLICY_CAN_GROW | GridConstraints.SIZEPOLICY_WANT_GROW,
+            GridConstraints.SIZEPOLICY_CAN_SHRINK,
+            null, null, null));
+        panel2.add(vSpacer3, new GridConstraints(2, 0, 1, 1,
+            GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL,
+            GridConstraints.SIZEPOLICY_CAN_SHRINK,
+            GridConstraints.SIZEPOLICY_CAN_GROW | GridConstraints.SIZEPOLICY_WANT_GROW,
+            null, null, null));
+      }
+      content.add(panel2, new GridConstraints(1, 0, 1, 1,
+          GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH,
+          GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+          GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+          null, null, null));
+
+      //======== panel3 ========
+      {
+        panel3.setBorder(new TitledBorder(new EtchedBorder(), bundle.getString("MTForm.panel3.border")));
+        panel3.setLayout(new GridLayoutManager(3, 2, new Insets(0, 0, 0, 0), -1, -1));
+
+        //---- isWallpaperSetCheckbox ----
+        isWallpaperSetCheckbox.setLabel(bundle.getString("MTForm.isWallpaperSetCheckbox.label"));
+        isWallpaperSetCheckbox.setText(bundle.getString("MTForm.isWallpaperSetCheckbox.text"));
+        isWallpaperSetCheckbox.setToolTipText("Whether to allow setting a custom wallpaper for the IDE");
+        panel3.add(isWallpaperSetCheckbox, new GridConstraints(0, 0, 1, 1,
+            GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE,
+            GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+            GridConstraints.SIZEPOLICY_FIXED,
+            null, null, null));
+        panel3.add(hSpacer3, new GridConstraints(0, 1, 1, 1,
+            GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL,
+            GridConstraints.SIZEPOLICY_CAN_GROW | GridConstraints.SIZEPOLICY_WANT_GROW,
+            GridConstraints.SIZEPOLICY_CAN_SHRINK,
+            null, null, null));
+
+        //---- isMaterialDesignCheckbox ----
+        isMaterialDesignCheckbox.setLabel(bundle.getString("MTForm.isMaterialDesignCheckbox.label"));
+        isMaterialDesignCheckbox.setText(bundle.getString("MTForm.isMaterialDesignCheckbox.text"));
+        isMaterialDesignCheckbox.setToolTipText(bundle.getString("MTForm.isMaterialDesignCheckbox.toolTipText"));
+        panel3.add(isMaterialDesignCheckbox, new GridConstraints(2, 0, 1, 1,
+            GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE,
+            GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+            GridConstraints.SIZEPOLICY_FIXED,
+            null, null, null));
+      }
+      content.add(panel3, new GridConstraints(2, 0, 1, 1,
+          GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH,
+          GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+          GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+          null, null, null));
+    }
+    // JFormDesigner - End of component initialization  //GEN-END:initComponents
+  }
+  // JFormDesigner - End of variables declaration  //GEN-END:variables
 }
