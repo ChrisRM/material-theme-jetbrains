855e5af7416d1a227cbb563717ef642838bae3e0
mallowigi <heliosaian@gmail.com>
2017-06-16 23:53:49 +0300

    cleanup

--- src/main/java/com/chrisrm/idea/config/scope/MTScopeComponent.java
+++ src/main/java/com/chrisrm/idea/config/scope/MTScopeComponent.java
@@ -4,39 +4,21 @@ import com.chrisrm.idea.actions.MTDarkerTheme;
 import com.chrisrm.idea.actions.MTDefaultTheme;
 import com.chrisrm.idea.actions.MTLighterTheme;
 import com.chrisrm.idea.actions.MTPalenightTheme;
-import com.intellij.ide.ui.LafManager;
-import com.intellij.ide.ui.LafManagerListener;
 import com.intellij.openapi.components.AbstractProjectComponent;
 import com.intellij.openapi.components.ProjectComponent;
 import com.intellij.openapi.project.Project;
 import com.intellij.ui.FileColorManager;
 
 public class MTScopeComponent extends AbstractProjectComponent implements ProjectComponent {
-  private final LafManager lafManager;
-  private LafManagerListener lafManagerListener;
 
-  protected MTScopeComponent(Project project, LafManager lafManager) {
+  protected MTScopeComponent(Project project) {
     super(project);
-    this.lafManager = lafManager;
   }
 
   @Override
   public void initComponent() {
     addDisabledFileColors();
-    //    lafManagerListener = source -> addDisabledFileColors();
   }
-
-  //  @Override
-  //  public void projectOpened() {
-  //    addDisabledFileColors();
-  //    lafManager.addLafManagerListener(lafManagerListener);
-  //  }
-  //
-  //  @Override
-  //  public void projectClosed() {
-  //    addDisabledFileColors();
-  //    lafManager.removeLafManagerListener(lafManagerListener);
-  //  }
 
   private void addDisabledFileColors() {
     FileColorManager manager = FileColorManager.getInstance(myProject);
