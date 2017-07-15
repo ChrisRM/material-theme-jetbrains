978e46e3cb900f28ea8101595a299d84980cd958
mallowigi <heliosaian@gmail.com>
2017-07-15 03:33:34 +0300

    still working on actions

--- src/main/java/com/chrisrm/idea/actions/MTToggleProjectViewDecoratorsAction.java
+++ src/main/java/com/chrisrm/idea/actions/MTToggleProjectViewDecoratorsAction.java
@@ -1,5 +1,6 @@
 package com.chrisrm.idea.actions;
 
+import com.chrisrm.idea.MTConfig;
 import com.chrisrm.idea.themes.MTThemeManager;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
@@ -11,5 +12,10 @@ public class MTToggleProjectViewDecoratorsAction extends AnAction {
   public void actionPerformed(AnActionEvent e) {
     // TODO: insert action logic here
     MTThemeManager.getInstance().toggleProjectViewDecorators();
+  }
+
+  @Override
+  public void update(AnActionEvent e) {
+    e.getPresentation().setEnabled(MTConfig.getInstance().isUseMaterialIcons());
   }
 }
