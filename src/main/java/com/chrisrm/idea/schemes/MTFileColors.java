a3e670b7eb1db3082368eeaa7cc22f6f9b3e00aa
mallowigi <heliosaian@gmail.com>
2017-08-04 21:49:48 +0300

    MTFileColors support

--- src/main/java/com/chrisrm/idea/schemes/MTFileColors.java
+++ src/main/java/com/chrisrm/idea/schemes/MTFileColors.java
@@ -1,6 +1,5 @@
 package com.chrisrm.idea.schemes;
 
-import com.chrisrm.idea.MTConfig;
 import com.intellij.openapi.editor.colors.ColorKey;
 import com.intellij.openapi.editor.colors.EditorColorsManager;
 import com.intellij.openapi.editor.colors.EditorColorsScheme;
@@ -13,24 +12,24 @@ import java.awt.*;
 import java.util.HashMap;
 
 public final class MTFileColors {
-  public static final ColorKey NOT_CHANGED_IMMEDIATE = ColorKey.createColorKey("NOT_CHANGED_IMMEDIATE", ColorUtil.fromHex("#80CBC4"));
-  public static final ColorKey NOT_CHANGED_RECURSIVE = ColorKey.createColorKey("NOT_CHANGED_RECURSIVE", ColorUtil.fromHex("#80CBC4"));
-  public static final ColorKey DELETED = ColorKey.createColorKey("DELETED", ColorUtil.fromHex("#F77669"));
-  public static final ColorKey MODIFIED = ColorKey.createColorKey("MODIFIED", ColorUtil.fromHex("#80CBC4"));
-  public static final ColorKey ADDED = ColorKey.createColorKey("ADDED", ColorUtil.fromHex("#C3E887"));
-  public static final ColorKey MERGE = ColorKey.createColorKey("MERGE", ColorUtil.fromHex("#C792EA"));
-  public static final ColorKey UNKNOWN = ColorKey.createColorKey("UNKNOWN", ColorUtil.fromHex("#F77669"));
-  public static final ColorKey IGNORED = ColorKey.createColorKey("IGNORED", ColorUtil.fromHex("#B0BEC5"));
-  public static final ColorKey HIJACKED = ColorKey.createColorKey("HIJACKED", ColorUtil.fromHex("#FFCB6B"));
-  public static final ColorKey MERGED_WITH_CONFLICTS = ColorKey.createColorKey("MERGED_WITH_CONFLICTS", ColorUtil.fromHex("#BC3F3C"));
-  public static final ColorKey MERGED_WITH_BOTH_CONFLICTS = ColorKey.createColorKey("MERGED_WITH_BOTH_CONFLICTS", ColorUtil.fromHex
+  public static final ColorKey NOT_CHANGED_IMMEDIATE = ColorKey.createColorKey("MT_NOT_CHANGED_IMMEDIATE", ColorUtil.fromHex("#80CBC4"));
+  public static final ColorKey NOT_CHANGED_RECURSIVE = ColorKey.createColorKey("MT_NOT_CHANGED_RECURSIVE", ColorUtil.fromHex("#80CBC4"));
+  public static final ColorKey DELETED = ColorKey.createColorKey("MT_DELETED", ColorUtil.fromHex("#F77669"));
+  public static final ColorKey MODIFIED = ColorKey.createColorKey("MT_MODIFIED", ColorUtil.fromHex("#80CBC4"));
+  public static final ColorKey ADDED = ColorKey.createColorKey("MT_ADDED", ColorUtil.fromHex("#C3E887"));
+  public static final ColorKey MERGE = ColorKey.createColorKey("MT_MERGE", ColorUtil.fromHex("#C792EA"));
+  public static final ColorKey UNKNOWN = ColorKey.createColorKey("MT_UNKNOWN", ColorUtil.fromHex("#F77669"));
+  public static final ColorKey IGNORED = ColorKey.createColorKey("MT_IGNORED", ColorUtil.fromHex("#B0BEC5"));
+  public static final ColorKey HIJACKED = ColorKey.createColorKey("MT_HIJACKED", ColorUtil.fromHex("#FFCB6B"));
+  public static final ColorKey MERGED_WITH_CONFLICTS = ColorKey.createColorKey("MT_MERGED_WITH_CONFLICTS", ColorUtil.fromHex("#BC3F3C"));
+  public static final ColorKey MERGED_WITH_BOTH_CONFLICTS = ColorKey.createColorKey("MT_MERGED_WITH_BOTH_CONFLICTS", ColorUtil.fromHex
       ("#BC3F3C"));
-  public static final ColorKey MERGED_WITH_PROPERTY_CONFLICTS = ColorKey.createColorKey("MERGED_WITH_PROPERTY_CONFLICTS", ColorUtil
+  public static final ColorKey MERGED_WITH_PROPERTY_CONFLICTS = ColorKey.createColorKey("MT_MERGED_WITH_PROPERTY_CONFLICTS", ColorUtil
       .fromHex("#BC3F3C"));
-  public static final ColorKey DELETED_FROM_FS = ColorKey.createColorKey("DELETED_FROM_FS", ColorUtil.fromHex("#626669"));
-  public static final ColorKey SWITCHED = ColorKey.createColorKey("SWITCHED", ColorUtil.fromHex("#F77669"));
-  public static final ColorKey OBSOLETE = ColorKey.createColorKey("OBSOLETE", ColorUtil.fromHex("#FFCB6B"));
-  public static final ColorKey SUPPRESSED = ColorKey.createColorKey("SUPPRESSED", ColorUtil.fromHex("#3C3F41"));
+  public static final ColorKey DELETED_FROM_FS = ColorKey.createColorKey("MT_DELETED_FROM_FS", ColorUtil.fromHex("#626669"));
+  public static final ColorKey SWITCHED = ColorKey.createColorKey("MT_SWITCHED", ColorUtil.fromHex("#F77669"));
+  public static final ColorKey OBSOLETE = ColorKey.createColorKey("MT_OBSOLETE", ColorUtil.fromHex("#FFCB6B"));
+  public static final ColorKey SUPPRESSED = ColorKey.createColorKey("MT_SUPPRESSED", ColorUtil.fromHex("#3C3F41"));
 
   public static final HashMap<FileStatus, ColorKey> FILE_STATUS_COLOR_MAP;
 
@@ -48,7 +47,7 @@ public final class MTFileColors {
         color = EditorColorsManager.getInstance().getGlobalScheme().getDefaultForeground();
       }
       // Add to the map
-      FILE_STATUS_COLOR_MAP.put(allFileStatus, ColorKey.createColorKey(allFileStatus.getId(), color));
+      FILE_STATUS_COLOR_MAP.put(allFileStatus, ColorKey.createColorKey("MT_" + allFileStatus.getId(), color));
     }
 
   }
@@ -59,9 +58,9 @@ public final class MTFileColors {
   public static Color get(final FileStatus status) {
     final EditorColorsScheme globalScheme = EditorColorsManager.getInstance().getGlobalScheme();
 
-    if (status == FileStatus.MODIFIED) {
-      return ColorUtil.fromHex(MTConfig.getInstance().getAccentColor());
-    }
+    //    if (status == FileStatus.MODIFIED) {
+    //      return ColorUtil.fromHex(MTConfig.getInstance().getAccentColor());
+    //    }
 
     final ColorKey colorKey = MTFileColors.FILE_STATUS_COLOR_MAP.get(status);
     if (colorKey != null) {
