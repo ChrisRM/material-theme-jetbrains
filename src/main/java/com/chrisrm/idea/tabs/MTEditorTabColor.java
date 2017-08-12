7948fde29746fcf6c6ad5febcbc165575ec74dde
mallowigi <heliosaian@gmail.com>
2017-08-12 15:35:20 +0300

    refacotr MTEditorTabColor

--- src/main/java/com/chrisrm/idea/tabs/MTEditorTabColor.java
+++ src/main/java/com/chrisrm/idea/tabs/MTEditorTabColor.java
@@ -31,33 +31,16 @@ import com.chrisrm.idea.MTTheme;
 import com.intellij.openapi.fileEditor.impl.EditorTabColorProvider;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.ui.JBColor;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.awt.*;
 
 public final class MTEditorTabColor implements EditorTabColorProvider {
 
-  private static final Color COLOR_DARKER = new JBColor(new Color(33, 33, 33, 255), new Color(33, 33, 33, 255));
-  private static final Color COLOR_DEFAULT = new JBColor(new Color(38, 50, 56, 255), new Color(38, 50, 56, 255));
-  private static final Color COLOR_PALENIGHT = new JBColor(new Color(41, 45, 62, 255), new Color(41, 45, 62, 255));
-  private static final Color COLOR_LIGHTER = new JBColor(new Color(250, 250, 250, 255), new Color(250, 250, 250, 255));
-
-  @Nullable
   @Override
   public Color getEditorTabColor(@NotNull final Project project, @NotNull final VirtualFile virtualFile) {
     final MTTheme mtTheme = MTConfig.getInstance().getSelectedTheme();
 
-    switch (mtTheme) {
-      case DARKER:
-        return COLOR_DARKER;
-      case PALENIGHT:
-        return COLOR_PALENIGHT;
-      case LIGHTER:
-        return COLOR_LIGHTER;
-      default:
-        return COLOR_DEFAULT;
-    }
+    return mtTheme.getBackgroundColor();
   }
 }
