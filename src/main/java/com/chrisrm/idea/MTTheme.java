0b87de0f0433ef44f4d9e98e88a3eeb81041349d
mallowigi <heliosaian@gmail.com>
2017-08-29 10:26:45 +0300

    fix foreground for checkboxes

--- src/main/java/com/chrisrm/idea/MTTheme.java
+++ src/main/java/com/chrisrm/idea/MTTheme.java
@@ -70,7 +70,8 @@ public abstract class MTTheme implements LafTheme, Serializable {
       JBColor.setDark(this.isDark());
       IconLoader.setUseDarkIcons(this.isDark());
 
-
+      UIManager.put("intellijlaf.background", new ColorUIResource(0x8078a8));
+      UIManager.getDefaults().put("mt.custom.background", new ColorUIResource(0x80cbc4));
       buildResources(getBackgroundResources(), getBackgroundColorString());
       buildResources(getForegroundResources(), getForegroundColorString());
       buildResources(getTextResources(), getTextColorString());
@@ -296,7 +297,7 @@ public abstract class MTTheme implements LafTheme, Serializable {
         "ToolTip.background",
         "Spinner.background",
         "SplitPane.highlight",
-        "Panel.background",
+        //        "Panel.background",
         "SidePanel.background",
         "DialogWrapper.southPanelDivider",
         "OnePixelDivider.background",
@@ -316,6 +317,8 @@ public abstract class MTTheme implements LafTheme, Serializable {
         "textInactiveText",
         "infoText",
         "controlText",
+        "Label.foreground",
+        "CheckBox.foreground",
         "OptionPane.messageForeground",
         "Menu.foreground",
         "MenuItem.foreground",
