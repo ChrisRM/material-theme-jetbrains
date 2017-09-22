890006209e806b3cdc416be15fcc68293249f6e5
mallowigi <heliosaian@gmail.com>
2017-09-22 17:08:28 +0300

    bbuybygugyuguyv

--- docs/config/index.js
+++ docs/config/index.js
@@ -24,9 +24,9 @@ module.exports = {
   dev: {
     env: require('./dev.env'),
     port: 8080,
-    autoOpenBrowser: true,
+    autoOpenBrowser: false,
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/docs',
     proxyTable: {},
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
