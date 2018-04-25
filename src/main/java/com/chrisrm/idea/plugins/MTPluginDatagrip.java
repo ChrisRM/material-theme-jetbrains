8204851c7e2315343daeafddcf2e16bfc5d9a711
mallowigi <heliosaian@gmail.com>
2018-04-25 07:28:52 +0300

    datagrip

---
+++ src/main/java/com/chrisrm/idea/plugins/MTPluginDatagrip.java
@@ -0,0 +1,54 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2018 Chris Magnussen and Elior Boukhobza
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *
+ */
+
+package com.chrisrm.idea.plugins;
+
+import com.chrisrm.idea.utils.IconReplacer;
+import com.intellij.openapi.components.ApplicationComponent;
+import org.jetbrains.annotations.NotNull;
+
+public final class MTPluginDatagrip implements ApplicationComponent {
+
+  @Override
+  public void initComponent() {
+    try {
+      final Class<?> iconsClass = Class.forName("icons.DatabaseIcons", false, getClass().getClassLoader());
+      IconReplacer.replaceIcons(iconsClass, "/icons/plugins/datagrip/");
+    } catch (final ClassNotFoundException e) {
+      // Suppress
+    }
+  }
+
+  @Override
+  public void disposeComponent() {
+  }
+
+  @Override
+  @NotNull
+  public String getComponentName() {
+    return "MTPluginRuby";
+  }
+}
