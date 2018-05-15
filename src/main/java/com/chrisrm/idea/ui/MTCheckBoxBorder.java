cc8f33fcd736580f59c37075ccaa1222eb2f8774
mallowigi <heliosaian@gmail.com>
2018-05-16 00:17:02 +0300

    fix checkbox

--- src/main/java/com/chrisrm/idea/ui/MTCheckBoxBorder.java
+++ src/main/java/com/chrisrm/idea/ui/MTCheckBoxBorder.java
@@ -36,18 +36,17 @@ import java.awt.*;
  * @author Konstantin Bulenkov
  */
 public final class MTCheckBoxBorder extends DarculaCheckBoxBorder implements Border, UIResource {
-    @Override
-    public void paintBorder(final Component c, final Graphics g, final int x, final int y, final int width, final int height) {
-    }
+  @Override
+  public void paintBorder(final Component c, final Graphics g, final int x, final int y, final int width, final int height) {
+  }
 
-    @Override
-    public Insets getBorderInsets(final Component c) {
-        final int a = 2;
-        return JBUI.insets(a, a, a, a).asUIResource();
-    }
+  @Override
+  public Insets getBorderInsets(final Component c) {
+    return JBUI.insets(2, 0, 2, 0).asUIResource();
+  }
 
-    @Override
-    public boolean isBorderOpaque() {
-        return false;
-    }
+  @Override
+  public boolean isBorderOpaque() {
+    return false;
+  }
 }
