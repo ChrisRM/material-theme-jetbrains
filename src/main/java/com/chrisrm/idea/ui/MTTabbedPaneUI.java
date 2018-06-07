865884eb30e04dc8d95f292c6ba7745293cfea11
Elior <elior.b@dynamicyield.com>
2018-06-07 22:44:37 +0300

    fix tabbed pane

--- src/main/java/com/chrisrm/idea/ui/MTTabbedPaneUI.java
+++ src/main/java/com/chrisrm/idea/ui/MTTabbedPaneUI.java
@@ -1,36 +1,37 @@
 /*
- * The MIT License (MIT)
+ *  The MIT License (MIT)
  *
- * Copyright (c) 2018 Chris Magnussen and Elior Boukhobza
+ *  Copyright (c) 2018 Chris Magnussen and Elior Boukhobza
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
  *
  */
 
 package com.chrisrm.idea.ui;
 
 import com.intellij.util.ObjectUtils;
+import sun.swing.SwingUtilities2;
 
 import javax.swing.*;
-import javax.swing.plaf.ComponentUI;
-import javax.swing.plaf.basic.BasicTabbedPaneUI;
+import javax.swing.plaf.*;
+import javax.swing.plaf.basic.*;
+import javax.swing.text.*;
 import java.awt.*;
 
 public class MTTabbedPaneUI extends BasicTabbedPaneUI {
@@ -66,6 +67,37 @@ public class MTTabbedPaneUI extends BasicTabbedPaneUI {
       case TOP:
       default:
         g.fillRect(x + 1, y + 1, w - 3, h - 1);
+    }
+  }
+
+  @Override
+  protected void paintText(final Graphics g, final int tabPlacement,
+                           final Font font, final FontMetrics metrics, final int tabIndex,
+                           final String title, final Rectangle textRect,
+                           final boolean isSelected) {
+
+    g.setFont(font);
+
+    final View v = getTextViewForTab(tabIndex);
+    if (v != null) {
+      // html
+      v.paint(g, textRect);
+    } else {
+      // plain text
+      final int mnemIndex = tabPane.getDisplayedMnemonicIndexAt(tabIndex);
+
+      if (tabPane.isEnabled() && tabPane.isEnabledAt(tabIndex)) {
+        final Color selectedFg = UIManager.getColor("TabbedPane.selectedForeground");
+        final Color fg = isSelected ? selectedFg : tabPane.getForegroundAt(tabIndex);
+
+        g.setColor(fg);
+        SwingUtilities2.drawStringUnderlineCharAt(tabPane, g, title, mnemIndex, textRect.x, textRect.y + metrics.getAscent());
+      } else { // tab disabled
+        g.setColor(tabPane.getBackgroundAt(tabIndex).brighter());
+        SwingUtilities2.drawStringUnderlineCharAt(tabPane, g, title, mnemIndex, textRect.x, textRect.y + metrics.getAscent());
+        g.setColor(tabPane.getBackgroundAt(tabIndex).darker());
+        SwingUtilities2.drawStringUnderlineCharAt(tabPane, g, title, mnemIndex, textRect.x - 1, textRect.y + metrics.getAscent() - 1);
+      }
     }
   }
 }
