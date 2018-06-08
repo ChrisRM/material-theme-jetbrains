c5b997d4fee208abb03fc6f48f78961bd32179af
mallowigi <heliosaian@gmail.com>
2018-06-08 23:38:22 +0300

    addGroup hack

--- src/main/java/com/chrisrm/idea/MTHackComponent.java
+++ src/main/java/com/chrisrm/idea/MTHackComponent.java
@@ -1,31 +1,33 @@
 /*
- *  The MIT License (MIT)
+ * The MIT License (MIT)
  *
- *  Copyright (c) 2018 Chris Magnussen and Elior Boukhobza
+ * Copyright (c) 2018 Chris Magnussen and Elior Boukhobza
  *
- *  Permission is hereby granted, free of charge, to any person obtaining a copy
- *  of this software and associated documentation files (the "Software"), to deal
- *  in the Software without restriction, including without limitation the rights
- *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *  copies of the Software, and to permit persons to whom the Software is
- *  furnished to do so, subject to the following conditions:
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- *  The above copyright notice and this permission notice shall be included in all
- *  copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
  *
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- *  SOFTWARE.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
  *
  */
 
 package com.chrisrm.idea;
 
 import com.intellij.ide.navigationToolbar.NavBarIdeView;
+import com.intellij.ide.plugins.PluginManagerConfigurable;
 import com.intellij.ide.util.PropertiesComponent;
 import com.intellij.openapi.actionSystem.impl.ChameleonAction;
 import com.intellij.openapi.components.ApplicationComponent;
@@ -56,6 +58,43 @@ public class MTHackComponent implements ApplicationComponent {
     hackFlatWelcomeFrame();
     hackPopupBorder();
     hackDarculaTabsPainter();
+    hackPluginManagerNew();
+  }
+
+  private static void hackPluginManagerNew() {
+    // Hack method
+    try {
+      final ClassPool cp = new ClassPool(true);
+      cp.insertClassPath(new ClassClassPath(PluginManagerConfigurable.class));
+      final CtClass ctClass = cp.get("com.intellij.ide.plugins.PluginManagerConfigurableNew$PluginsGroupComponent");
+
+      final CtMethod addGroup = ctClass.getDeclaredMethod("addGroup", new CtClass[]{
+          cp.get("com.intellij.ide.plugins.PluginManagerConfigurableNew$PluginsGroup"),
+          cp.get("int")
+      });
+      addGroup.instrument(new ExprEditor() {
+        @Override
+        public void edit(final MethodCall m) throws CannotCompileException {
+          if (m.getMethodName().equals("setForeground")) {
+            final String fgColor = "javax.swing.UIManager.getColor(\"List.foreground\")";
+
+            m.replace(String.format("{ $1 = %s; $_ = $proceed($$); }", fgColor));
+          }
+        }
+
+        @Override
+        public void edit(final NewExpr e) throws CannotCompileException {
+          if (e.getClassName().contains("OpaquePanel")) {
+            final String bgColor = "javax.swing.UIManager.getColor(\"List.background\")";
+
+            e.replace(String.format("{ $2 = %s; $_ = $proceed($$); }", bgColor, bgColor));
+          }
+        }
+      });
+      ctClass.toClass();
+    } catch (final Exception e) {
+      e.printStackTrace();
+    }
   }
 
   public MTHackComponent() {
