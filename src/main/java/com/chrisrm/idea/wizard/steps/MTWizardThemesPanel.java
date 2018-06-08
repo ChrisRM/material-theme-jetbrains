11be4bde1b3bfaf90ccbbb00b0f69b8560a3cadb
mallowigi <heliosaian@gmail.com>
2018-06-08 23:38:22 +0300

    wip

--- src/main/java/com/chrisrm/idea/wizard/steps/MTWizardThemesPanel.java
+++ src/main/java/com/chrisrm/idea/wizard/steps/MTWizardThemesPanel.java
@@ -34,11 +34,13 @@ import com.chrisrm.idea.MTConfig;
 import com.chrisrm.idea.MTThemeFacade;
 import com.chrisrm.idea.MTThemeManager;
 import com.chrisrm.idea.MTThemes;
+import com.intellij.CommonBundle;
 import com.intellij.ide.customize.AbstractCustomizeWizardStep;
-import com.intellij.uiDesigner.core.GridConstraints;
-import com.intellij.uiDesigner.core.GridLayoutManager;
+import com.intellij.openapi.options.OptionsBundle;
+import net.miginfocom.swing.MigLayout;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 
@@ -58,6 +60,14 @@ public class MTWizardThemesPanel extends AbstractCustomizeWizardStep {
   @Override
   protected String getHTMLHeader() {
     return "<html><body><h2>Set UI theme</h2>&nbsp;</body></html>";
+  }
+
+  @Override
+  public String getHTMLFooter() {
+    return "UI theme can be changed later in " +
+        CommonBundle.settingsTitle()
+        + " | " + OptionsBundle.message("configurable.group.appearance.settings.display.name")
+        + " | " + "Appearance";
   }
 
   private void selectTheme(final MTThemeFacade theme) {
@@ -113,151 +123,248 @@ public class MTWizardThemesPanel extends AbstractCustomizeWizardStep {
     // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
     // Generated using JFormDesigner non-commercial license
     selectThemeLabel = new JLabel();
+    panel1 = new JPanel();
     oceanicButton = new JRadioButton();
+    label1 = new JLabel();
+    panel11 = new JPanel();
     darkerButton = new JRadioButton();
-    palenightButton = new JRadioButton();
+    label2 = new JLabel();
+    panel10 = new JPanel();
     lighterButton = new JRadioButton();
+    label11 = new JLabel();
+    panel12 = new JPanel();
+    palenightButton = new JRadioButton();
+    label10 = new JLabel();
+    panel3 = new JPanel();
     deepoceanButton = new JRadioButton();
+    label9 = new JLabel();
+    panel4 = new JPanel();
     monokaiButton = new JRadioButton();
+    label8 = new JLabel();
+    panel5 = new JPanel();
     arcdarkButton = new JRadioButton();
+    label7 = new JLabel();
+    panel6 = new JPanel();
     onedarkButton = new JRadioButton();
+    label6 = new JLabel();
+    panel7 = new JPanel();
     onelightButton = new JRadioButton();
+    label5 = new JLabel();
+    panel8 = new JPanel();
     solarizedDarkButton = new JRadioButton();
+    label4 = new JLabel();
+    panel9 = new JPanel();
     solarizedLightButton = new JRadioButton();
+    label3 = new JLabel();
 
     //======== this ========
     setPreferredSize(null);
     setMinimumSize(null);
     setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
-    setLayout(new GridLayoutManager(7, 2, new Insets(0, 0, 0, 0), 0, 0));
+    setLayout(new MigLayout(
+        "insets 0,hidemode 3,gap 0 0",
+        // columns
+        "[grow 1,fill]" +
+            "[fill]",
+        // rows
+        "[grow 1,fill]" +
+            "[fill]" +
+            "[fill]" +
+            "[fill]" +
+            "[fill]" +
+            "[fill]" +
+            "[fill]"));
 
     //---- selectThemeLabel ----
     selectThemeLabel.setText("Select a theme:");
-    add(selectThemeLabel, new GridConstraints(0, 0, 1, 2,
-        GridConstraints.ANCHOR_NORTHWEST, GridConstraints.FILL_NONE,
-        GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
-        GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
-        null, null, null, 2));
+    selectThemeLabel.setBorder(new EmptyBorder(5, 5, 5, 5));
+    add(selectThemeLabel, "cell 0 0 2 1,align left top,grow 0 0");
 
-    //---- oceanicButton ----
-    oceanicButton.setText("Oceanic");
-    oceanicButton.setHorizontalAlignment(SwingConstants.LEFT);
-    oceanicButton.setIcon(new ImageIcon(getClass().getResource("/wizard/oceanic.png")));
-    oceanicButton.addActionListener(e -> oceanicButtonActionPerformed(e));
-    add(oceanicButton, new GridConstraints(1, 0, 1, 1,
-        GridConstraints.ANCHOR_NORTHWEST, GridConstraints.FILL_BOTH,
-        GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
-        GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
-        null, null, null));
+    //======== panel1 ========
+    {
+      panel1.setBorder(new EmptyBorder(5, 5, 5, 5));
+      panel1.setLayout(new BoxLayout(panel1, BoxLayout.Y_AXIS));
 
-    //---- darkerButton ----
-    darkerButton.setText("Darker");
-    darkerButton.setHorizontalAlignment(SwingConstants.LEFT);
-    darkerButton.setIcon(new ImageIcon(getClass().getResource("/wizard/darker.png")));
-    darkerButton.addActionListener(e -> darkerButtonActionPerformed(e));
-    add(darkerButton, new GridConstraints(1, 1, 1, 1,
-        GridConstraints.ANCHOR_NORTHWEST, GridConstraints.FILL_BOTH,
-        GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
-        GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
-        null, null, null));
+      //---- oceanicButton ----
+      oceanicButton.setText("Oceanic");
+      oceanicButton.setHorizontalAlignment(SwingConstants.LEFT);
+      oceanicButton.addActionListener(e -> oceanicButtonActionPerformed(e));
+      panel1.add(oceanicButton);
 
-    //---- palenightButton ----
-    palenightButton.setText("Palenight");
-    palenightButton.setIcon(new ImageIcon(getClass().getResource("/wizard/palenight.png")));
-    palenightButton.addActionListener(e -> palenightButtonActionPerformed(e));
-    add(palenightButton, new GridConstraints(2, 0, 1, 1,
-        GridConstraints.ANCHOR_NORTHWEST, GridConstraints.FILL_BOTH,
-        GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
-        GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
-        null, null, null));
+      //---- label1 ----
+      label1.setIcon(new ImageIcon(getClass().getResource("/wizard/oceanic.png")));
+      panel1.add(label1);
+    }
+    add(panel1, "cell 0 1,align center center,grow 0 0");
 
-    //---- lighterButton ----
-    lighterButton.setText("Lighter");
-    lighterButton.setIcon(new ImageIcon(getClass().getResource("/wizard/lighter.png")));
-    lighterButton.addActionListener(e -> lighterButtonActionPerformed(e));
-    add(lighterButton, new GridConstraints(2, 1, 1, 1,
-        GridConstraints.ANCHOR_NORTHWEST, GridConstraints.FILL_BOTH,
-        GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
-        GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
-        null, null, null));
+    //======== panel11 ========
+    {
+      panel11.setBorder(new EmptyBorder(5, 5, 5, 5));
+      panel11.setLayout(new BoxLayout(panel11, BoxLayout.Y_AXIS));
 
-    //---- deepoceanButton ----
-    deepoceanButton.setText("Deep Ocean");
-    deepoceanButton.setIcon(new ImageIcon(getClass().getResource("/wizard/deepocean.png")));
-    deepoceanButton.addActionListener(e -> deepoceanButtonActionPerformed(e));
-    add(deepoceanButton, new GridConstraints(3, 0, 1, 1,
-        GridConstraints.ANCHOR_NORTHWEST, GridConstraints.FILL_BOTH,
-        GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
-        GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
-        null, null, null));
+      //---- darkerButton ----
+      darkerButton.setText("Darker");
+      darkerButton.setHorizontalAlignment(SwingConstants.LEFT);
+      darkerButton.addActionListener(e -> darkerButtonActionPerformed(e));
+      panel11.add(darkerButton);
 
-    //---- monokaiButton ----
-    monokaiButton.setText("Monokai");
-    monokaiButton.setIcon(new ImageIcon(getClass().getResource("/wizard/monokai.png")));
-    monokaiButton.addActionListener(e -> monokaiButtonActionPerformed(e));
-    add(monokaiButton, new GridConstraints(3, 1, 1, 1,
-        GridConstraints.ANCHOR_NORTHWEST, GridConstraints.FILL_BOTH,
-        GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
-        GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
-        null, null, null));
+      //---- label2 ----
+      label2.setIcon(new ImageIcon(getClass().getResource("/wizard/darker.png")));
+      panel11.add(label2);
+    }
+    add(panel11, "cell 1 1,align center center,grow 0 0");
 
-    //---- arcdarkButton ----
-    arcdarkButton.setText("Arc Dark");
-    arcdarkButton.setIcon(new ImageIcon(getClass().getResource("/wizard/arcdark.png")));
-    arcdarkButton.addActionListener(e -> arcdarkButtonActionPerformed(e));
-    add(arcdarkButton, new GridConstraints(4, 0, 1, 1,
-        GridConstraints.ANCHOR_NORTHWEST, GridConstraints.FILL_BOTH,
-        GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
-        GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
-        null, null, null));
+    //======== panel10 ========
+    {
+      panel10.setBorder(new EmptyBorder(5, 5, 5, 5));
+      panel10.setLayout(new BoxLayout(panel10, BoxLayout.Y_AXIS));
 
-    //---- onedarkButton ----
-    onedarkButton.setText("One Dark");
-    onedarkButton.setIcon(new ImageIcon(getClass().getResource("/wizard/onedark.png")));
-    onedarkButton.addActionListener(e -> onedarkButtonActionPerformed(e));
-    add(onedarkButton, new GridConstraints(4, 1, 1, 1,
-        GridConstraints.ANCHOR_NORTHWEST, GridConstraints.FILL_BOTH,
-        GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
-        GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
-        null, null, null));
+      //---- lighterButton ----
+      lighterButton.setText("Lighter");
+      lighterButton.addActionListener(e -> lighterButtonActionPerformed(e));
+      panel10.add(lighterButton);
 
-    //---- onelightButton ----
-    onelightButton.setText("One Light");
-    onelightButton.setIcon(new ImageIcon(getClass().getResource("/wizard/onelight.png")));
-    onelightButton.addActionListener(e -> onelightButtonActionPerformed(e));
-    add(onelightButton, new GridConstraints(5, 0, 1, 1,
-        GridConstraints.ANCHOR_NORTHWEST, GridConstraints.FILL_BOTH,
-        GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
-        GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
-        null, null, null));
+      //---- label11 ----
+      label11.setIcon(new ImageIcon(getClass().getResource("/wizard/lighter.png")));
+      panel10.add(label11);
+    }
+    add(panel10, "cell 1 2,align center center,grow 0 0");
 
-    //---- solarizedDarkButton ----
-    solarizedDarkButton.setText("Solarized Dark");
-    solarizedDarkButton.setHorizontalAlignment(SwingConstants.LEFT);
-    solarizedDarkButton.setIcon(new ImageIcon(getClass().getResource("/wizard/solarizeddark.png")));
-    solarizedDarkButton.addActionListener(e -> solarizedDarkButtonActionPerformed(e));
-    add(solarizedDarkButton, new GridConstraints(5, 1, 1, 1,
-        GridConstraints.ANCHOR_NORTHWEST, GridConstraints.FILL_BOTH,
-        GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
-        GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
-        null, null, null));
+    //======== panel12 ========
+    {
+      panel12.setBorder(new EmptyBorder(5, 5, 5, 5));
+      panel12.setLayout(new BoxLayout(panel12, BoxLayout.Y_AXIS));
 
-    //---- solarizedLightButton ----
-    solarizedLightButton.setText("Solarized Light");
-    solarizedLightButton.setIcon(new ImageIcon(getClass().getResource("/wizard/solarizedlight.png")));
-    solarizedLightButton.addActionListener(e -> solarizedLightButtonActionPerformed(e));
-    add(solarizedLightButton, new GridConstraints(6, 0, 1, 1,
-        GridConstraints.ANCHOR_NORTHWEST, GridConstraints.FILL_BOTH,
-        GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
-        GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
-        null, null, null));
+      //---- palenightButton ----
+      palenightButton.setText("Palenight");
+      palenightButton.addActionListener(e -> palenightButtonActionPerformed(e));
+      panel12.add(palenightButton);
+
+      //---- label10 ----
+      label10.setIcon(new ImageIcon(getClass().getResource("/wizard/palenight.png")));
+      panel12.add(label10);
+    }
+    add(panel12, "cell 0 2,align center center,grow 0 0");
+
+    //======== panel3 ========
+    {
+      panel3.setBorder(new EmptyBorder(5, 5, 5, 5));
+      panel3.setLayout(new BoxLayout(panel3, BoxLayout.Y_AXIS));
+
+      //---- deepoceanButton ----
+      deepoceanButton.setText("Deep Ocean");
+      deepoceanButton.addActionListener(e -> deepoceanButtonActionPerformed(e));
+      panel3.add(deepoceanButton);
+
+      //---- label9 ----
+      label9.setIcon(new ImageIcon(getClass().getResource("/wizard/deepocean.png")));
+      panel3.add(label9);
+    }
+    add(panel3, "cell 0 3,align center center,grow 0 0");
+
+    //======== panel4 ========
+    {
+      panel4.setBorder(new EmptyBorder(5, 5, 5, 5));
+      panel4.setLayout(new BoxLayout(panel4, BoxLayout.Y_AXIS));
+
+      //---- monokaiButton ----
+      monokaiButton.setText("Monokai");
+      monokaiButton.addActionListener(e -> monokaiButtonActionPerformed(e));
+      panel4.add(monokaiButton);
+
+      //---- label8 ----
+      label8.setIcon(new ImageIcon(getClass().getResource("/wizard/monokai.png")));
+      panel4.add(label8);
+    }
+    add(panel4, "cell 1 3,align center center,grow 0 0");
+
+    //======== panel5 ========
+    {
+      panel5.setBorder(new EmptyBorder(5, 5, 5, 5));
+      panel5.setLayout(new BoxLayout(panel5, BoxLayout.Y_AXIS));
+
+      //---- arcdarkButton ----
+      arcdarkButton.setText("Arc Dark");
+      arcdarkButton.addActionListener(e -> arcdarkButtonActionPerformed(e));
+      panel5.add(arcdarkButton);
+
+      //---- label7 ----
+      label7.setIcon(new ImageIcon(getClass().getResource("/wizard/arcdark.png")));
+      panel5.add(label7);
+    }
+    add(panel5, "cell 0 4,align center center,grow 0 0");
+
+    //======== panel6 ========
+    {
+      panel6.setBorder(new EmptyBorder(5, 5, 5, 5));
+      panel6.setLayout(new BoxLayout(panel6, BoxLayout.Y_AXIS));
+
+      //---- onedarkButton ----
+      onedarkButton.setText("One Dark");
+      onedarkButton.addActionListener(e -> onedarkButtonActionPerformed(e));
+      panel6.add(onedarkButton);
+
+      //---- label6 ----
+      label6.setIcon(new ImageIcon(getClass().getResource("/wizard/onedark.png")));
+      panel6.add(label6);
+    }
+    add(panel6, "cell 1 4,align center center,grow 0 0");
+
+    //======== panel7 ========
+    {
+      panel7.setBorder(new EmptyBorder(5, 5, 5, 5));
+      panel7.setLayout(new BoxLayout(panel7, BoxLayout.Y_AXIS));
+
+      //---- onelightButton ----
+      onelightButton.setText("One Light");
+      onelightButton.addActionListener(e -> onelightButtonActionPerformed(e));
+      panel7.add(onelightButton);
+
+      //---- label5 ----
+      label5.setIcon(new ImageIcon(getClass().getResource("/wizard/onelight.png")));
+      panel7.add(label5);
+    }
+    add(panel7, "cell 0 5");
+
+    //======== panel8 ========
+    {
+      panel8.setBorder(new EmptyBorder(5, 5, 5, 5));
+      panel8.setLayout(new BoxLayout(panel8, BoxLayout.Y_AXIS));
+
+      //---- solarizedDarkButton ----
+      solarizedDarkButton.setText("Solarized Dark");
+      solarizedDarkButton.setHorizontalAlignment(SwingConstants.LEFT);
+      solarizedDarkButton.addActionListener(e -> solarizedDarkButtonActionPerformed(e));
+      panel8.add(solarizedDarkButton);
+
+      //---- label4 ----
+      label4.setIcon(new ImageIcon(getClass().getResource("/wizard/solarizeddark.png")));
+      panel8.add(label4);
+    }
+    add(panel8, "cell 1 5");
+
+    //======== panel9 ========
+    {
+      panel9.setBorder(new EmptyBorder(5, 5, 5, 5));
+      panel9.setLayout(new BoxLayout(panel9, BoxLayout.Y_AXIS));
+
+      //---- solarizedLightButton ----
+      solarizedLightButton.setText("Solarized Light");
+      solarizedLightButton.addActionListener(e -> solarizedLightButtonActionPerformed(e));
+      panel9.add(solarizedLightButton);
+
+      //---- label3 ----
+      label3.setIcon(new ImageIcon(getClass().getResource("/wizard/solarizedlight.png")));
+      panel9.add(label3);
+    }
+    add(panel9, "cell 0 6,align center center,grow 0 0");
 
     //---- selectedTheme ----
     final ButtonGroup selectedTheme = new ButtonGroup();
     selectedTheme.add(oceanicButton);
     selectedTheme.add(darkerButton);
-    selectedTheme.add(palenightButton);
     selectedTheme.add(lighterButton);
+    selectedTheme.add(palenightButton);
     selectedTheme.add(deepoceanButton);
     selectedTheme.add(monokaiButton);
     selectedTheme.add(arcdarkButton);
@@ -271,17 +378,39 @@ public class MTWizardThemesPanel extends AbstractCustomizeWizardStep {
   // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
   // Generated using JFormDesigner non-commercial license
   private JLabel selectThemeLabel;
+  private JPanel panel1;
   private JRadioButton oceanicButton;
+  private JLabel label1;
+  private JPanel panel11;
   private JRadioButton darkerButton;
-  private JRadioButton palenightButton;
+  private JLabel label2;
+  private JPanel panel10;
   private JRadioButton lighterButton;
+  private JLabel label11;
+  private JPanel panel12;
+  private JRadioButton palenightButton;
+  private JLabel label10;
+  private JPanel panel3;
   private JRadioButton deepoceanButton;
+  private JLabel label9;
+  private JPanel panel4;
   private JRadioButton monokaiButton;
+  private JLabel label8;
+  private JPanel panel5;
   private JRadioButton arcdarkButton;
+  private JLabel label7;
+  private JPanel panel6;
   private JRadioButton onedarkButton;
+  private JLabel label6;
+  private JPanel panel7;
   private JRadioButton onelightButton;
+  private JLabel label5;
+  private JPanel panel8;
   private JRadioButton solarizedDarkButton;
+  private JLabel label4;
+  private JPanel panel9;
   private JRadioButton solarizedLightButton;
+  private JLabel label3;
   // JFormDesigner - End of variables declaration  //GEN-END:variables
 
 }
