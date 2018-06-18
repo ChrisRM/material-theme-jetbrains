06dad7b78ef36cd166b6a2b9c4a95465a05d05f6
Elior <elior.b@dynamicyield.com>
2018-06-18 18:11:21 +0300

    fix primary color darcula

--- src/main/java/com/chrisrm/idea/MTAbstractTheme.java
+++ src/main/java/com/chrisrm/idea/MTAbstractTheme.java
@@ -1,26 +1,25 @@
 /*
- * The MIT License (MIT)
+ *  The MIT License (MIT)
  *
- * Copyright (c) 2018 Chris Magnussen and Elior Boukhobza
+ *  Copyright (c) 2018 Chris Magnussen and Elior Boukhobza
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
  *
  */
 
@@ -42,7 +41,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
-import javax.swing.plaf.ColorUIResource;
+import javax.swing.plaf.*;
 import java.awt.*;
 import java.io.Serializable;
 
@@ -181,7 +180,7 @@ public abstract class MTAbstractTheme implements Serializable, MTThemeable {
    * Get resources using the background color
    */
   protected String[] getBackgroundResources() {
-    return new String[]{
+    return new String[] {
         //        "Menu.background",
         "window",
         "activeCaption",
@@ -269,7 +268,7 @@ public abstract class MTAbstractTheme implements Serializable, MTThemeable {
    * Get resources using the foreground color
    */
   protected String[] getForegroundResources() {
-    return new String[]{
+    return new String[] {
         "text",
         "textText",
         "textInactiveText",
@@ -324,7 +323,7 @@ public abstract class MTAbstractTheme implements Serializable, MTThemeable {
    * Get resources using the label color
    */
   protected String[] getTextResources() {
-    return new String[]{
+    return new String[] {
         "Menu.acceleratorForeground",
         "text",
         "textText",
@@ -354,7 +353,7 @@ public abstract class MTAbstractTheme implements Serializable, MTThemeable {
    * Get resources using the selection background color
    */
   protected String[] getSelectionBackgroundResources() {
-    return new String[]{
+    return new String[] {
         "Menu.selectionBackground",
         "MenuItem.selectionBackground",
         "RadioButtonMenuItem.selectionBackground",
@@ -375,7 +374,7 @@ public abstract class MTAbstractTheme implements Serializable, MTThemeable {
    * Get resources using the selection foreground color
    */
   protected String[] getSelectionForegroundResources() {
-    return new String[]{
+    return new String[] {
         "Menu.selectionForeground",
         "Menu.acceleratorSelectionForeground",
         "MenuItem.selectionForeground",
@@ -408,7 +407,7 @@ public abstract class MTAbstractTheme implements Serializable, MTThemeable {
    * Get resources using the button color
    */
   protected String[] getButtonColorResource() {
-    return new String[]{
+    return new String[] {
         "Button.mt.color1",
         "Button.mt.color2",
         "Button.mt.background",
@@ -434,7 +433,7 @@ public abstract class MTAbstractTheme implements Serializable, MTThemeable {
    * Get resources using the secondary background color
    */
   protected String[] getSecondaryBackgroundResources() {
-    return new String[]{
+    return new String[] {
         "inactiveCaption",
         "ToolWindow.header.active.background",
         "ToolWindow.header.border.background",
@@ -452,7 +451,7 @@ public abstract class MTAbstractTheme implements Serializable, MTThemeable {
    * Get resources using the disabled color
    */
   protected String[] getDisabledResources() {
-    return new String[]{
+    return new String[] {
         "MenuItem.disabledForeground",
         "ComboBox.disabledForeground",
         "CheckBox.darcula.disabledBorderColor1",
@@ -482,7 +481,7 @@ public abstract class MTAbstractTheme implements Serializable, MTThemeable {
    * Get resources using the contrast color
    */
   protected String[] getContrastResources() {
-    return new String[]{
+    return new String[] {
         "Table.stripedBackground",
         "ToolWindow.header.tab.selected.background",
         "ToolWindow.header.tab.selected.active.background",
@@ -508,7 +507,7 @@ public abstract class MTAbstractTheme implements Serializable, MTThemeable {
    * Get resources using the table/button selection color
    */
   protected String[] getTableSelectedResources() {
-    return new String[]{
+    return new String[] {
         "Table.selectionBackground",
         "TextField.selectionBackground",
         "PasswordField.selectionBackground",
@@ -534,7 +533,7 @@ public abstract class MTAbstractTheme implements Serializable, MTThemeable {
    * Get resources using the second border color
    */
   protected String[] getSecondBorderResources() {
-    return new String[]{
+    return new String[] {
         "Button.darcula.shadowColor",
         "Separator.foreground",
         "TabbedPane.highlight",
@@ -556,7 +555,7 @@ public abstract class MTAbstractTheme implements Serializable, MTThemeable {
    * Get resources using the highlight color
    */
   protected String[] getHighlightResources() {
-    return new String[]{
+    return new String[] {
         "Focus.color",
         "TextField.separatorColor",
         "ProgressBar.halfColor",
@@ -581,7 +580,7 @@ public abstract class MTAbstractTheme implements Serializable, MTThemeable {
    * Get resources using the tree selected row color
    */
   protected String[] getTreeSelectionResources() {
-    return new String[]{
+    return new String[] {
         "Tree.selectionBackground"
     };
   }
@@ -595,7 +594,7 @@ public abstract class MTAbstractTheme implements Serializable, MTThemeable {
    * Get notifications colors resources
    */
   protected String[] getNotificationsResources() {
-    return new String[]{
+    return new String[] {
         "Notifications.background",
         "Notifications.borderColor"
     };
@@ -664,8 +663,8 @@ public abstract class MTAbstractTheme implements Serializable, MTThemeable {
   public Color getPrimaryColor() {
     final Color defaultValue = MTUiUtils.getColor(
         DEFAULT_PRIMARY,
-        ObjectUtils.notNull(UIManager.getColor("darcula.background"), new ColorUIResource(0x3c3f41)),
-        ObjectUtils.notNull(UIManager.getColor("intellijlaf.background"), new ColorUIResource(0xe8e8e8)));
+        new ColorUIResource(0x87939A),
+        new ColorUIResource(0xAEB9C0));
     return ObjectUtils.notNull(UIManager.getColor("material.primaryColor"), defaultValue);
   }
 
