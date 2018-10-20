4b6a3e8724c274d1ddd4d3fba34d5c5ac9358db9
Elior <elior.b@dynamicyield.com>
2018-10-21 00:04:38 +0300

    allow more customization when Material components is disabled

--- src/main/java/com/chrisrm/idea/MTLafInstaller.java
+++ src/main/java/com/chrisrm/idea/MTLafInstaller.java
@@ -1,26 +1,25 @@
 /*
- * The MIT License (MIT)
+ *  The MIT License (MIT)
  *
- * Copyright (c) 2018 Chris Magnussen and Elior Boukhobza
+ *  Copyright (c) 2018 Chris Magnussen and Elior Boukhobza
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
  *
  */
 
@@ -64,25 +63,26 @@ public class MTLafInstaller {
   }
 
   protected void installMTDefaults(final UIDefaults defaults) {
+    replaceStatusBar(defaults);
+    replaceTree(defaults);
+    replaceSelectedIndicator(defaults);
+    replaceDropdowns(defaults);
+    replaceTableHeaders(defaults);
+    replaceIcons(defaults);
+    replaceRootPane(defaults);
+    replaceMenus(defaults);
+
     if (mtConfig.getIsMaterialDesign()) {
       replaceButtons(defaults);
       replaceTextFields(defaults);
-      replaceDropdowns(defaults);
       replaceProgressBar(defaults);
-      replaceTree(defaults);
-      replaceSelectedIndicator(defaults);
-      replaceTableHeaders(defaults);
       replaceTables(defaults);
-      replaceStatusBar(defaults);
       replaceSpinners(defaults);
       replaceCheckboxes(defaults);
       replaceRadioButtons(defaults);
       replaceSliders(defaults);
       replaceTextAreas(defaults);
       replaceTabbedPanes(defaults);
-      replaceIcons(defaults);
-      replaceRootPane(defaults);
-      replaceMenus(defaults);
       modifyRegistry(defaults);
     }
   }
