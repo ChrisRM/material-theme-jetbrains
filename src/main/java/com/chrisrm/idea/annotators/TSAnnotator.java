8356058ebcd7e339c761f77e0b5da8863e271f80
Elior <elior.b@dynamicyield.com>
2018-10-30 18:43:59 +0200

    fix javacolors

--- src/main/java/com/chrisrm/idea/annotators/TSAnnotator.java
+++ src/main/java/com/chrisrm/idea/annotators/TSAnnotator.java
@@ -33,23 +33,20 @@ import com.intellij.psi.PsiElement;
 import com.intellij.util.ObjectUtils;
 import org.jetbrains.annotations.NotNull;
 
-public class TSAnnotator extends JSAnnotator implements Annotator {
+public class TSAnnotator extends BaseAnnotator implements Annotator {
   private static final TextAttributesKey JSKEYWORD = ObjectUtils.notNull(TextAttributesKey.find("JS.KEYWORD"),
-      DefaultLanguageHighlighterColors.KEYWORD);
+                                                                         DefaultLanguageHighlighterColors.KEYWORD);
   public static final TextAttributesKey PRIVATE = TextAttributesKey.createTextAttributesKey("TS.PRIVATE_PUBLIC", JSKEYWORD);
 
   @Override
   public TextAttributesKey getKeywordKind(@NotNull final PsiElement element) {
-    TextAttributesKey kind = super.getKeywordKind(element);
-    if (kind == null) {
-      switch (element.getText()) {
-        case "public":
-        case "protected":
-        case "private":
-          kind = PRIVATE;
-          break;
-      }
-      return kind;
+    TextAttributesKey kind = null;
+    switch (element.getText()) {
+      case "public":
+      case "protected":
+      case "private":
+        kind = PRIVATE;
+        break;
     }
     return kind;
   }
--- src/main/java/com/chrisrm/idea/annotators/settings/JavaColorSettings.java
+++ src/main/java/com/chrisrm/idea/annotators/settings/JavaColorSettings.java
@@ -28,8 +28,8 @@ package com.chrisrm.idea.annotators.settings;
 
 import com.chrisrm.idea.annotators.JavaAnnotator;
 import com.intellij.icons.AllIcons;
-import com.intellij.ide.highlighter.JavaHighlightingColors;
 import com.intellij.lang.Language;
+import com.intellij.openapi.editor.DefaultLanguageHighlighterColors;
 import com.intellij.openapi.editor.colors.CodeInsightColors;
 import com.intellij.openapi.editor.colors.TextAttributesKey;
 import com.intellij.openapi.fileTypes.SyntaxHighlighter;
@@ -58,7 +58,7 @@ public class JavaColorSettings extends BaseColorSettings {
   private static final TextAttributesKey STATIC_FINAL = JavaAnnotator.STATIC_FINAL;
 
   static {
-    JAVA_ATTRIBUTES = new AttributesDescriptor[]{
+    JAVA_ATTRIBUTES = new AttributesDescriptor[] {
         new AttributesDescriptor("Keywords: this, super", JavaColorSettings.THIS_SUPER),
         new AttributesDescriptor("Keywords: private, public, protected", JavaColorSettings.MODIFIER),
         new AttributesDescriptor("Keywords: static, final", JavaColorSettings.STATIC_FINAL),
@@ -70,7 +70,8 @@ public class JavaColorSettings extends BaseColorSettings {
   private static Map<String, TextAttributesKey> createAdditionalHlAttrs() {
     final Map<String, TextAttributesKey> descriptors = new THashMap<>();
 
-    descriptors.put("field", JavaHighlightingColors.INSTANCE_FIELD_ATTRIBUTES);
+    descriptors.put("field", ObjectUtils.notNull(TextAttributesKey.find("INSTANCE_FIELD_ATTRIBUTES"),
+                                                 DefaultLanguageHighlighterColors.INSTANCE_FIELD));
     descriptors.put("unusedField", CodeInsightColors.NOT_USED_ELEMENT_ATTRIBUTES);
     descriptors.put("error", CodeInsightColors.ERRORS_ATTRIBUTES);
     descriptors.put("warning", CodeInsightColors.WARNINGS_ATTRIBUTES);
@@ -78,36 +79,65 @@ public class JavaColorSettings extends BaseColorSettings {
     descriptors.put("server_problems", CodeInsightColors.GENERIC_SERVER_ERROR_OR_WARNING);
     descriptors.put("server_duplicate", CodeInsightColors.DUPLICATE_FROM_SERVER);
     descriptors.put("unknownType", CodeInsightColors.WRONG_REFERENCES_ATTRIBUTES);
-    descriptors.put("localVar", JavaHighlightingColors.LOCAL_VARIABLE_ATTRIBUTES);
-    descriptors.put("reassignedLocalVar", JavaHighlightingColors.REASSIGNED_LOCAL_VARIABLE_ATTRIBUTES);
-    descriptors.put("reassignedParameter", JavaHighlightingColors.REASSIGNED_PARAMETER_ATTRIBUTES);
-    descriptors.put("implicitAnonymousParameter", JavaHighlightingColors.IMPLICIT_ANONYMOUS_CLASS_PARAMETER_ATTRIBUTES);
-    descriptors.put("static", JavaHighlightingColors.STATIC_FIELD_ATTRIBUTES);
-    descriptors.put("static_final", JavaHighlightingColors.STATIC_FINAL_FIELD_ATTRIBUTES);
+    descriptors.put("localVar", ObjectUtils.notNull(TextAttributesKey.find("LOCAL_VARIABLE_ATTRIBUTES"),
+                                                    DefaultLanguageHighlighterColors.LOCAL_VARIABLE));
+    descriptors.put("reassignedLocalVar", ObjectUtils.notNull(TextAttributesKey.find("REASSIGNED_LOCAL_VARIABLE_ATTRIBUTES"),
+                                                              DefaultLanguageHighlighterColors.REASSIGNED_LOCAL_VARIABLE));
+    descriptors.put("reassignedParameter", ObjectUtils.notNull(TextAttributesKey.find("REASSIGNED_PARAMETER_ATTRIBUTES);"),
+                                                               DefaultLanguageHighlighterColors.REASSIGNED_PARAMETER));
+    descriptors.put("implicitAnonymousParameter",
+                    ObjectUtils.notNull(TextAttributesKey.find("IMPLICIT_ANONYMOUS_CLASS_PARAMETER_ATTRIBUTES);"),
+                                        DefaultLanguageHighlighterColors.CLASS_NAME));
+    descriptors.put("static", ObjectUtils.notNull(TextAttributesKey.find("STATIC_FIELD_ATTRIBUTES);"),
+                                                  DefaultLanguageHighlighterColors.STATIC_FIELD));
+    descriptors.put("static_final", ObjectUtils.notNull(TextAttributesKey.find("STATIC_FINAL_FIELD_ATTRIBUTES);"),
+                                                        DefaultLanguageHighlighterColors.STATIC_FIELD));
     descriptors.put("deprecated", CodeInsightColors.DEPRECATED_ATTRIBUTES);
     descriptors.put("for_removal", CodeInsightColors.MARKED_FOR_REMOVAL_ATTRIBUTES);
-    descriptors.put("constructorCall", JavaHighlightingColors.CONSTRUCTOR_CALL_ATTRIBUTES);
-    descriptors.put("constructorDeclaration", JavaHighlightingColors.CONSTRUCTOR_DECLARATION_ATTRIBUTES);
-    descriptors.put("methodCall", JavaHighlightingColors.METHOD_CALL_ATTRIBUTES);
-    descriptors.put("methodDeclaration", JavaHighlightingColors.METHOD_DECLARATION_ATTRIBUTES);
-    descriptors.put("static_method", JavaHighlightingColors.STATIC_METHOD_ATTRIBUTES);
-    descriptors.put("abstract_method", JavaHighlightingColors.ABSTRACT_METHOD_ATTRIBUTES);
-    descriptors.put("inherited_method", JavaHighlightingColors.INHERITED_METHOD_ATTRIBUTES);
-    descriptors.put("param", JavaHighlightingColors.PARAMETER_ATTRIBUTES);
-    descriptors.put("lambda_param", JavaHighlightingColors.LAMBDA_PARAMETER_ATTRIBUTES);
-    descriptors.put("class", JavaHighlightingColors.CLASS_NAME_ATTRIBUTES);
-    descriptors.put("anonymousClass", JavaHighlightingColors.ANONYMOUS_CLASS_NAME_ATTRIBUTES);
-    descriptors.put("typeParameter", JavaHighlightingColors.TYPE_PARAMETER_NAME_ATTRIBUTES);
-    descriptors.put("abstractClass", JavaHighlightingColors.ABSTRACT_CLASS_NAME_ATTRIBUTES);
-    descriptors.put("interface", JavaHighlightingColors.INTERFACE_NAME_ATTRIBUTES);
-    descriptors.put("enum", JavaHighlightingColors.ENUM_NAME_ATTRIBUTES);
-    descriptors.put("annotationName", JavaHighlightingColors.ANNOTATION_NAME_ATTRIBUTES);
-    descriptors.put("annotationAttributeName", JavaHighlightingColors.ANNOTATION_ATTRIBUTE_NAME_ATTRIBUTES);
-    descriptors.put("javadocTagValue", JavaHighlightingColors.DOC_COMMENT_TAG_VALUE);
-    descriptors.put("instanceFinalField", JavaHighlightingColors.INSTANCE_FINAL_FIELD_ATTRIBUTES);
-    descriptors.put("staticallyConstImported", JavaHighlightingColors.STATIC_FINAL_FIELD_IMPORTED_ATTRIBUTES);
-    descriptors.put("staticallyImported", JavaHighlightingColors.STATIC_FIELD_IMPORTED_ATTRIBUTES);
-    descriptors.put("static_imported_method", JavaHighlightingColors.STATIC_METHOD_CALL_IMPORTED_ATTRIBUTES);
+    descriptors.put("constructorCall", ObjectUtils.notNull(TextAttributesKey.find("CONSTRUCTOR_CALL_ATTRIBUTES);"),
+                                                           DefaultLanguageHighlighterColors.FUNCTION_CALL));
+    descriptors.put("constructorDeclaration", ObjectUtils.notNull(TextAttributesKey.find("CONSTRUCTOR_DECLARATION_ATTRIBUTES);"),
+                                                                  DefaultLanguageHighlighterColors.FUNCTION_DECLARATION));
+    descriptors.put("methodCall", ObjectUtils.notNull(TextAttributesKey.find("METHOD_CALL_ATTRIBUTES);"),
+                                                      DefaultLanguageHighlighterColors.FUNCTION_CALL));
+    descriptors.put("methodDeclaration", ObjectUtils.notNull(TextAttributesKey.find("METHOD_DECLARATION_ATTRIBUTES);"),
+                                                             DefaultLanguageHighlighterColors.FUNCTION_DECLARATION));
+    descriptors.put("static_method", ObjectUtils.notNull(TextAttributesKey.find("STATIC_METHOD_ATTRIBUTES);"),
+                                                         DefaultLanguageHighlighterColors.STATIC_METHOD));
+    descriptors.put("abstract_method", ObjectUtils.notNull(TextAttributesKey.find("ABSTRACT_METHOD_ATTRIBUTES);"),
+                                                           DefaultLanguageHighlighterColors.FUNCTION_CALL));
+    descriptors.put("inherited_method", ObjectUtils.notNull(TextAttributesKey.find("INHERITED_METHOD_ATTRIBUTES);"),
+                                                            DefaultLanguageHighlighterColors.FUNCTION_CALL));
+    descriptors.put("param", ObjectUtils.notNull(TextAttributesKey.find("PARAMETER_ATTRIBUTES);"),
+                                                 DefaultLanguageHighlighterColors.PARAMETER));
+    descriptors.put("lambda_param", ObjectUtils.notNull(TextAttributesKey.find("LAMBDA_PARAMETER_ATTRIBUTES);"),
+                                                        DefaultLanguageHighlighterColors.PARAMETER));
+    descriptors.put("class", ObjectUtils.notNull(TextAttributesKey.find("CLASS_NAME_ATTRIBUTES);"),
+                                                 DefaultLanguageHighlighterColors.CLASS_NAME));
+    descriptors.put("anonymousClass", ObjectUtils.notNull(TextAttributesKey.find("ANONYMOUS_CLASS_NAME_ATTRIBUTES);"),
+                                                          DefaultLanguageHighlighterColors.CLASS_NAME));
+    descriptors.put("typeParameter", ObjectUtils.notNull(TextAttributesKey.find("TYPE_PARAMETER_NAME_ATTRIBUTES);"),
+                                                         DefaultLanguageHighlighterColors.PARAMETER));
+    descriptors.put("abstractClass", ObjectUtils.notNull(TextAttributesKey.find("ABSTRACT_CLASS_NAME_ATTRIBUTES);"),
+                                                         DefaultLanguageHighlighterColors.CLASS_NAME));
+    descriptors.put("interface", ObjectUtils.notNull(TextAttributesKey.find("INTERFACE_NAME_ATTRIBUTES);"),
+                                                     DefaultLanguageHighlighterColors.INTERFACE_NAME));
+    descriptors.put("enum", ObjectUtils.notNull(TextAttributesKey.find("ENUM_NAME_ATTRIBUTES);"),
+                                                DefaultLanguageHighlighterColors.CLASS_NAME));
+    descriptors.put("annotationName", ObjectUtils.notNull(TextAttributesKey.find("ANNOTATION_NAME_ATTRIBUTES);"),
+                                                          DefaultLanguageHighlighterColors.METADATA));
+    descriptors.put("annotationAttributeName", ObjectUtils.notNull(TextAttributesKey.find("ANNOTATION_ATTRIBUTE_NAME_ATTRIBUTES);"),
+                                                                   DefaultLanguageHighlighterColors.METADATA));
+    descriptors.put("javadocTagValue", ObjectUtils.notNull(TextAttributesKey.find("DOC_COMMENT_TAG_VALUE);"),
+                                                           DefaultLanguageHighlighterColors.DOC_COMMENT_TAG_VALUE));
+    descriptors.put("instanceFinalField", ObjectUtils.notNull(TextAttributesKey.find("INSTANCE_FINAL_FIELD_ATTRIBUTES);"),
+                                                              DefaultLanguageHighlighterColors.INSTANCE_FIELD));
+    descriptors.put("staticallyConstImported", ObjectUtils.notNull(TextAttributesKey.find("STATIC_FINAL_FIELD_IMPORTED_ATTRIBUTES);"),
+                                                                   DefaultLanguageHighlighterColors.STATIC_FIELD));
+    descriptors.put("staticallyImported", ObjectUtils.notNull(TextAttributesKey.find("STATIC_FIELD_IMPORTED_ATTRIBUTES);"),
+                                                              DefaultLanguageHighlighterColors.STATIC_FIELD));
+    descriptors.put("static_imported_method", ObjectUtils.notNull(TextAttributesKey.find("STATIC_METHOD_CALL_IMPORTED_ATTRIBUTES);"),
+                                                                  DefaultLanguageHighlighterColors.STATIC_METHOD));
 
     descriptors.put("keyword", JavaColorSettings.JAVA_KEYWORD);
     descriptors.put("this", JavaColorSettings.THIS_SUPER);
@@ -135,16 +165,16 @@ public class JavaColorSettings extends BaseColorSettings {
   public String getDemoText() {
     return
         "public class <class>SomeClass</class> extends <class>BaseClass</class> {\n" +
-            "  <modifier>private</modifier> <sf>static final</sf> <field>field</field> = null;\n" +
-            "  <modifier>protected</modifier> <sf>final</sf> <field>otherField</field>;\n\n" +
-            "  <modifier>public</modifier> <constructorDeclaration>SomeClass</constructorDeclaration>(<interface>AnInterface</interface> " +
-            "<param>param1</param>, int[] <reassignedParameter>reassignedParam</reassignedParameter>,\n" +
-            "                  int <param>param2</param>\n" +
-            "                  int <param>param3</param>) {\n" +
-            "    <this>super</this>(<param>param1</param>);\n" +
-            "    <this>this</this>.<warning>field</warning> = <param>param1</param>;\n" +
-            "  }\n " +
-            "}\n";
+        "  <modifier>private</modifier> <sf>static final</sf> <field>field</field> = null;\n" +
+        "  <modifier>protected</modifier> <sf>final</sf> <field>otherField</field>;\n\n" +
+        "  <modifier>public</modifier> <constructorDeclaration>SomeClass</constructorDeclaration>(<interface>AnInterface</interface> " +
+        "<param>param1</param>, int[] <reassignedParameter>reassignedParam</reassignedParameter>,\n" +
+        "                  int <param>param2</param>\n" +
+        "                  int <param>param3</param>) {\n" +
+        "    <this>super</this>(<param>param1</param>);\n" +
+        "    <this>this</this>.<warning>field</warning> = <param>param1</param>;\n" +
+        "  }\n " +
+        "}\n";
   }
 
   @Nullable
