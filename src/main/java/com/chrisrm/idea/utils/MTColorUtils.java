6d776ce9b5fbaf015d7d2cac06a53766259c5efa
mallowigi <heliosaian@gmail.com>
2018-11-19 23:17:48 +0200

    fix contrast, for good this time

--- src/main/java/com/chrisrm/idea/utils/MTColorUtils.java
+++ src/main/java/com/chrisrm/idea/utils/MTColorUtils.java
@@ -77,13 +77,13 @@ public enum MTColorUtils {
       return color;
     }
 
-    return isDark ?
-           ColorUtil.brighter(color, HC_FG_TONES) :
-           ColorUtil.darker(color, HC_FG_TONES);
+    return new ColorUIResource(isDark ?
+                               ColorUtil.brighter(color, HC_FG_TONES) :
+                               ColorUtil.darker(color, HC_FG_TONES));
   }
 
   @SuppressWarnings("unused")
-  private static String contrastifyBackground(final boolean isDark, final String colorString, final boolean isNotHighContrast) {
+  public static String contrastifyBackground(final boolean isDark, final String colorString, final boolean isNotHighContrast) {
     if (isNotHighContrast) {
       return colorString;
     }
@@ -94,13 +94,13 @@ public enum MTColorUtils {
   }
 
   @SuppressWarnings("unused")
-  public static Color contrastifyBackground(final boolean isDark, final Color color, final boolean isNotHighContrast) {
+  public static Color contrastifyBackground(final boolean isDark, final ColorUIResource color, final boolean isNotHighContrast) {
     if (isNotHighContrast) {
       return color;
     }
 
-    return isDark ?
-           ColorUtil.darker(color, HC_BG_TONES) :
-           ColorUtil.brighter(color, HC_BG_TONES);
+    return new ColorUIResource(isDark ?
+                               ColorUtil.darker(color, HC_BG_TONES) :
+                               ColorUtil.brighter(color, HC_BG_TONES));
   }
 }
