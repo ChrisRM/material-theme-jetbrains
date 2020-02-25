5ac4b8cb206f6c415890658b2347728991f02250
mallowigi <heliosaian@gmail.com>
2020-02-25 21:32:41 +0200

    fix theme not being persisted

--- src/main/java/com/mallowigi/idea/MTLafComponent.java
+++ src/main/java/com/mallowigi/idea/MTLafComponent.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2019 Chris Magnussen and Elior Boukhobza
+ * Copyright (c) 2015 - 2020 Chris Magnussen and Elior Boukhobza
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -218,7 +218,7 @@ public final class MTLafComponent implements AppLifecycleListener {
     //    MTThemeManager.updateFileIcons();
     MTSelectedTreeIndicatorImpl.resetCache();
 
-    activateTheme(true);
+    MTThemeManager.setLookAndFeel(MTConfig.getInstance().getSelectedTheme());
 
     ApplicationManager.getApplication().runWriteAction(UIReplacer::patchUI);
 
