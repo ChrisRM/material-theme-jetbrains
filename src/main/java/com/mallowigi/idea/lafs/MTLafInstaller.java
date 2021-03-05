69631c9143c329530ef6fbedd7a2ac60d2bb8185
Elior <heliosaian@gmail.com>
2021-03-05 16:56:38 +0200

    fix external themes missing defaults

--- src/main/java/com/mallowigi/idea/lafs/MTLafInstaller.java
+++ src/main/java/com/mallowigi/idea/lafs/MTLafInstaller.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2015-2020 Elior "Mallowigi" Boukhobza
+ * Copyright (c) 2015-2021 Elior "Mallowigi" Boukhobza
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -119,7 +119,7 @@ public class MTLafInstaller {
     defaults.put("Component.focusWidth", 2);
     defaults.put("CompletionPopup.nonFocusedState", false);
     defaults.put("CellEditor.border.width", 2);
-    defaults.put("Window.border", "1,1,1,1,FF0000");
+    defaults.put("Window.border", "1,1,1,1,000000");
     defaults.put("Tree.border", "1,1,1,1");
     defaults.put("ListUI", "com.intellij.ui.components.WideSelectionListUI");
 
--- src/main/java/com/mallowigi/idea/lafs/MTNativeLaf.java
+++ src/main/java/com/mallowigi/idea/lafs/MTNativeLaf.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2019 Chris Magnussen and Elior Boukhobza
+ * Copyright (c) 2015-2021 Elior "Mallowigi" Boukhobza
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -79,6 +79,7 @@ public final class MTNativeLaf extends DarculaLaf {
 
   @Override
   protected void loadDefaults(final UIDefaults defaults) {
+    MTLafInstaller.loadDefaults(defaults);
     ((UIThemeBasedLookAndFeelInfo) currentLookAndFeel).installTheme(defaults, true);
   }
 
