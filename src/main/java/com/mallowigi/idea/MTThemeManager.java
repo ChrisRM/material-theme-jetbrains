1b1aea6699c0dad821458617e56f562e4c396c06
Elior <heliosaian@gmail.com>
2021-04-23 15:23:07 +0300

    try to fix issue where applying the same theme results on resetting to darcula

--- src/main/java/com/mallowigi/idea/MTThemeManager.java
+++ src/main/java/com/mallowigi/idea/MTThemeManager.java
@@ -400,6 +400,10 @@ public final class MTThemeManager implements Disposable {
     final UIManager.LookAndFeelInfo lafInfo = ContainerUtil.find(lafManager.getInstalledLookAndFeels(),
       lookAndFeelInfo -> lookAndFeelInfo.getName().equals(selectedTheme.getThemeName()));
 
+    if (selectedTheme == MTConfig.getInstance().getSelectedTheme()) {
+      return;
+    }
+
     MTChangeLAFAnimator.showSnapshot();
     if (lafInfo != null) {
       lafManager.setCurrentLookAndFeel(lafInfo);
